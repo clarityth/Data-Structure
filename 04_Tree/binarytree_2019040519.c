dfd3f5d796fbb6c9953463183a5957d7913c95f2
TaehyungKim <netchip8@naver.com>
2022-05-23 18:31:11 +0900

    Add BinaryTree

--- 04_Tree/binarytree_2019040519.c
+++ 04_Tree/binarytree_2019040519.c
@@ -4,15 +4,15 @@
 BinaryTree* bt_create(int data)
 {
   BinaryTree *new = (BinaryTree *)malloc(sizeof(BinaryTree));
-    new->data = data;
-    new->lchild = NULL;
-    new->rchild = NULL;
-    return new;
+  new->data = data;
+  new->lchild = NULL;
+  new->rchild = NULL;
+  return new;
 }
 
 bool bt_is_empty(BinaryTree* bt)
 {
-  if (!(bt->lchild) && !(bt->rchild))
+  if (bt->lchild == NULL && bt->rchild == NULL)
     return true;
   else
     return false;
@@ -21,11 +21,21 @@ bool bt_is_empty(BinaryTree* bt)
 BinaryTree* bt_make(BinaryTree* root, BinaryTree* bt1, BinaryTree* bt2)
 {
   /*
-   * 예외처리 방법: bt1, bt2가 다른 트리의 서브트리인지 고려하기 위해, BinaryTree 구조체에 parent를 추가한다.
+   * bt1, bt2가 다른 트리의 서브트리인지 고려하려면, BinaryTree 구조체에 parent를 추가한다.
    */
-  root->lchild = bt1;
-  root->rchild = bt2;
-  return root;
+ 
+  if (root){
+    if (bt1)
+      root->lchild = bt1;
+    if (bt2)
+      root->rchild = bt2;
+    return root;
+  }
+  // 예외 처리
+  else {
+    printf("NULL Pointer Exception");
+    return NULL;
+  }
 }
 
 void bt_print_preorder(BinaryTree* bt)
@@ -37,6 +47,7 @@ void bt_print_preorder(BinaryTree* bt)
     if (bt->rchild)
       bt_print_preorder(bt->rchild);
   }
+  // 예외 처리
   else {
     printf("NULL Pointer Exception");
     return;
@@ -52,6 +63,7 @@ void bt_print_postorder(BinaryTree* bt)
       bt_print_postorder(bt->rchild);
     printf("%d->", bt->data);
   }
+  // 예외 처리
   else {
     printf("NULL Pointer Exception");
     return;
@@ -67,6 +79,7 @@ void bt_print_inorder(BinaryTree* bt)
     if (bt->rchild)
       bt_print_inorder(bt->rchild);
   }
+  // 예외 처리
   else {
     printf("NULL Pointer Exception");
     return;
